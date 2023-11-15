09f375cf6a61c9c791efca072262305ef3941236
Carlo Wiesse <charliehpwiesse@gmail.com>
2023-11-15 09:21:41 +0800

    workspace setup changes

--- setup.sh
+++ setup.sh
@@ -7,7 +7,15 @@ if ! [ -d $TRAINING_DIR/ros_workspace/src ]; then
   mkdir -p $TRAINING_DIR/ros_workspace/src
   cd $TRAINING_DIR/ros_workspace
   vcs import src < $TRAINING_DIR/repos.txt
-  cd $TRAINING_DIR/ros_workspace/src/manipulation_training
-  cp -r students_copy/* .
-  rm -rf solutions/ students_copy/
+  cd $TRAINING_DIR/ros_workspace/src
+  rm -rf manipulation_training/
+fi
+
+if ! [ -d $TRAINING_DIR/day1_ws/src ]; then
+  mkdir -p $TRAINING_DIR/day1_ws/src
+  cd $TRAINING_DIR/day1_ws
+  vcs import src < $TRAINING_DIR/repos.txt
+  cd $TRAINING_DIR/day1_ws/src
+  cp -r manipulation_training/students_copy/* .
+  rm -rf ur_driver/ ur_description/ warehouse_ros_mongo/ manipulation_training/
 fi
